7fc1627c3afb3998de7a700ae8cc953126011dbd
huyixi <hi@huyixi.com>
2024-01-21 21:42:40 +0800

    Style: code wrap

--- themes/white/uno.config.ts
+++ themes/white/uno.config.ts
@@ -11,6 +11,17 @@ export default defineConfig({
           'border-left': '2px solid #ccc',
           'padding-left': '0.5rem',
           'font-style': 'none',
+        },
+        'pre': {
+          'border': '1px solid #ddd',
+          'border-radius': '4px',
+          'background': '#EEE !important',
+          'padding': '0.5em',
+        },
+        'pre code': {
+          'font-size': '12px',
+          'white-space': 'pre-wrap',
+          'overflow-wrap': 'break-word',
         }
       }
     })
